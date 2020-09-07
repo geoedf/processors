d3abc3b065fe6385c7d4fa5dfcac94e9c348c566
Rajesh Kalyanam <39563013+rkalyanapurdue@users.noreply.github.com>
2020-09-07 17:26:57 -0400

    Added CSV2HAR processor (#3)

---
+++ csv2har/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='csv2har',
+      version='0.1',
+      description='Processor for converting a SIMPLE csv file into a HAR file',
+      url='http://github.com/geoedf/csv2har',
+      author='Rajesh Kalyanam',
+      author_email='rkalyanapurdue@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False)
