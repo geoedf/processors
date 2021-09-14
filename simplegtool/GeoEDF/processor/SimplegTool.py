ce8c47a14b4a89b6122ba301fc2fb6513e7c8daa
junghawoo <jungha.woo@gmail.com>
2021-09-13 23:10:38 -0400

    added comments, docstring

--- simplegtool/GeoEDF/processor/SimplegTool.py
+++ simplegtool/GeoEDF/processor/SimplegTool.py
@@ -7,20 +7,52 @@ from geoedfframework.GeoEDFPlugin import GeoEDFPlugin
 import geopandas as gpd
 import glob
 import os
-import subprocess
 from os import walk
+import subprocess
 import re
-import glob
 import shutil
 
 from pathlib import Path
-""" Module for implementing the SimplegTool processor. 
-"""
 
 class SimplegTool(GeoEDFPlugin):
-    # input directory or shapefile params are XOR
-    # shapefile will take precedence
-    # if end is provided, period also needs to be provided
+    """ Module for running 02_data_proc binary file that generates
+        two database files ( LANDDATA.har, DATACHKS.har).
+
+        SimplegTool requires two parameters:
+        1) har_input_dir : directory where INC, POP, QCROP, VCROP, QLAND data exist
+        2) target_year : base year
+
+        An economic model (02_data_proc.tab) is compiled into a Fortran code by GEMPACK
+        program(copsmodels.com), and then transforms into an executable named 02_data_proc.
+
+        The resulting executable/binary takes a command file (CMF) that instructs
+        model to load inputs(INC, POP, QCROP, and so on). 
+        User needs to edit the cmf file in order to select the base year(which is 
+        same as target_year,one of the required parameters). User specified target_year
+        will be automatically set in the command file through edit_CMF_target_path().
+
+        Remember that base year or target_year must fall within start_year and 
+        end_year of the SimpleDataClean processor.
+
+        Input Data files  (see input folder)!    
+        --------------------------------------     
+        FILE INC_DAT   # real GDP (in USD 2005) #;
+        FILE POP_DAT   # population (in numbers) #;
+        FILE QCROP_DAT # aggregate crop output (in corn. eq. MTs) #;
+        FILE VCROP_DAT # Value of aggregate crop output (in USD) #;
+        FILE QLAND_DAT # cropland (in ha) #;
+        ------------------------------------------
+
+        Note that SimplegTool processor modifies CSV2HAR processor's output directory.
+        It copies YYYY_variable.har files to YYYY/variable.har because the command file
+        reads inputs from the baHar files left in target_path will be returned to the users as final output.
+                Turns out that copying files using subprocess with wildcard causes 
+                either failure or shell execution concern.
+                This function copies all har files in subdirectory of the target path 
+                to the destination directory.se_year/variable.har.
+        Please refer to move_har_to_YYYY() for details.
+
+    """
     __optional_params = []
     __required_params = ['har_input_dir', 'target_year']
 
@@ -55,59 +87,94 @@ class SimplegTool(GeoEDFPlugin):
     # assume this method is called only when all params have been fully instantiated
     def process(self):
 
+        # restructure CSV2HAR output directory structure
         self.move_har_to_YYYY()
         
+        #Singualarity container cannot modify its file system.
+        #The binary modifies the same directory where it sits in.
+        #Therefore /simpleg directory is copied to the writable output directory
+        #of the SimplegTool processor, and the final HAR files are copied to the topmost
+        #directory of the SimplegTool output directory.
         self.copy_simpleg_to_target_path()
         
+        # the directory simpleg binary is located at
         exec_dir = self.target_path+"/simpleg/02_data_proc"
+        # the executable path 
         exec_path = exec_dir +"/02_data_proc"
+        # the command file path 
         cmf_path  = exec_dir +"/02_data_proc.cmf"
+        # directory that model binary produces .har files
         output_har_dir = exec_dir+"/out"
         
+        #make command file to look up given base year's input data
         self.edit_CMF_target_path(cmf_path)
         
         
         try:
-            #subprocess.call(["/bin/cp","-r", "/simpleg", self.target_path])
             subprocess.call([exec_path, "-cmf", cmf_path], cwd= exec_dir)
+            # Not working 
             # wildcard character is literally interpreted and cause no such file error
             #subprocess.check_output(["/bin/cp", output_har_dir+"/*.har", self.target_path])
             
-            # working code 
+            # Working  
             #subprocess.check_output("/bin/cp"+"  "+output_har_dir+"/*.har"+"  "+self.target_path, shell=True)
             
+            # want to copy all har files model binary produced
             har_files_to_copy = output_har_dir+"/*.har"
+            
             self.copy_hars_to_destination(har_files_to_copy, self.target_path)
             
             
             #subprocess.call(["/bin/rm","-rf", self.target_path+"/simpleg/"])
 
         except:
-            raise GeoEDFError('Error occurred when running Shapefile2GeoJSON processor')
+            raise GeoEDFError('Error occurred when running SimplegTool processor')
 
             
-    # Har files left in target_path will be returned to the users as final output
-    # Copying files using subprocess with wildcard causes either failure or shell execution concern.
-    # This function copies all har files in subdirectory of the target path to the topmost of the the target path
+
     def copy_hars_to_destination(self, pattern, destination_dir):
+        """ Copy har files matching the given pattern to the destination directory
+    
+            Har files left in target_path will be returned to the users as final output.
+            Turns out that copying files using subprocess with wildcard causes 
+            either failure or shell execution concern.
+            This function copies all har files in subdirectory of the target path 
+            to the destination directory.
+        """
         for file in glob.glob(pattern):
             print(file)
             shutil.copy(file, destination_dir)
             
         
         
-        
+
     def copy_simpleg_to_target_path(self):
+        """ Copy /simpleg directory of the Singularity container to output directory.
+    
+            This is to circumvent the read-only limitation of the Singularity container.
+            The directory /simpleg is copied to 
+            a temporary execution place, output directory of the SimplegTool processor,
+            and then executable is invoked.
+        """
         try:
             subprocess.call(["/bin/cp","-r", "/simpleg", self.target_path])
 
         except:
-            raise GeoEDFError('Error occurred when copying simpleg to the writable target path')
+            raise GeoEDFError('Error occurred when copying simpleg to the SimplegTool output directory')
+
+            
+            
 
     def move_har_to_YYYY(self):
+        """ Finds all YYYY_alphanumeric.har files and move them to YYYY directory.
+        
+            Year component is removed from the filename. 
+        """
+        
         fnames = []
         regex=r'(?P<year>[1-9][0-9]{3,3})\_(?P<varname>(\w)+).har'
 
+        # get a list of files in har_input_dir
         for (dirpath, dirnames, filenames) in walk(self.har_input_dir):
             print("dirpath", dirpath)
             print("filenames", filenames)
@@ -115,6 +182,7 @@ class SimplegTool(GeoEDFPlugin):
             print(fnames)
             break
 
+        # if YYYY_variable.har is found, copy it to YYYY/variable.har
         for filename in fnames:
             #print(filename)
             m = re.match(regex, filename)
@@ -128,20 +196,40 @@ class SimplegTool(GeoEDFPlugin):
                 dst_filepath = os.path.join(dirpath, m.group('year'), m.group('varname')+".har")
                 shutil.copy( src_filepath , dst_filepath)
         
-    # find predefined variable <YYYY_PATH>, and replace it with target_year's directory 
+
     def edit_CMF_target_path(self, cmf_path):
-        
-        target_year_path = os.path.join(self.har_input_dir,str(self.target_year)) 
+        """ find predefined variable <YYYY_PATH> in command file, 
+            and replace it with target_year's directory.
+
+            ex, FILE INC_DAT   = <YYYY_PATH>/INC.har;
+                may be replaced with 
+                FILE INC_DAT   = /data/37393048/3/2005/INC.har"
+        """
+        target_year_path = oHar files left in target_path will be returned to the users as final output.
+            Turns out that copying files using subprocess with wildcard causes 
+            either failure or shell execution concern.
+            This function copies all har files in subdirectory of the target path 
+            to the destination directory.s.path.join(self.har_input_dir,str(self.target_year)) 
         self.replace_string_in_file(cmf_path, "<YYYY_PATH>", target_year_path)
-        
+    
+    
+    
+   
     def replace_string_in_file(self, filename, pattern, new_string):
+        """ Update paths in the command file(CMF).
+    
+            Input HAR file paths are at the CSV2HAR output directory.
+            This function updates input HAR file paths in command file so that
+            it can correctly look up inputs. It finds all occurrence of "pattern" 
+            from "filename" and replace them with "new_string". 
+        """
         with open(filename, 'rt') as fin:
             s = fin.read()
 
-            if pattern not in s:
+            if pattern not in s:  # if not found, do not replace anything.
                 print('"{pattern}" not found in {filename}.'.format(**locals()))
                 return
 
-        with open(filename, 'wt') as fout:
+        with open(filename, 'wt') as fout: # if found, replace all <YYYY_PATH> with target year's directory
             s = s.replace(pattern, new_string )
             fout.write(s)
