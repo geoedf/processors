0cb6e82ba2e8dde5947982127b40fe089ef8199b
Jack Smith <smith1106@marshall.edu>
2022-03-28 07:49:57 -0500

    Add WQXWeb processor

---
+++ wqxweb/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name='wqxweb',
+      version='0.1',
+      description='Processor for submitting water quality data to WQX',
+      url='http://github.com/geoedf/wqxweb',
+      author='Jack Smith',
+      author_email='smith1106@marshak.edu',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=['dotenv'],
+      zip_safe=False)
